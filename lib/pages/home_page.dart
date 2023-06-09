83cad877854bab754cdfa698dc504db41d0f80bd
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-09 16:38:57 -0300

    commit 13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,7 +42,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: _Lista(lista: listaProdutos),
+      body: _Lista(lista: listaProdutosTeste),
     );
   }
 }
